049d16d3bb240b21ad02968061d73c10af27881d
yonePC <ys.mailaddress0929@gmail.com>
2021-09-15 02:10:38 +0000

    App1 8章

--- 4.App1/CARAVAN/app/controllers/blogs_controller.rb
+++ 4.App1/CARAVAN/app/controllers/blogs_controller.rb
@@ -10,25 +10,34 @@ class BlogsController < ApplicationController
   def new
     @blog = Blog.new
   end
-  
+
   def create
-    blog = Blog.new(blog_params)
-    blog.save
-    redirect_to blog_path(blog.id)
-  end  
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blog_path(@blog.id)
+    else
+      render :new
+    end
+  end
 
   def edit
     @blog = Blog.find(params[:id])
   end
-  
+
   def update
     blog = Blog.find(params[:id])
     blog.update(blog_params)
     redirect_to blog_path(blog)
-  end  
-  
+  end
+
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
+  end
+
   private
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
-  end  
+  end
 end
