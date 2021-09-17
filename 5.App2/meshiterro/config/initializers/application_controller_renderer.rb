c018592ff1b526249db7a2a4998bfa2ed3ddc67f
yonePC <ys.mailaddress0929@gmail.com>
2021-09-17 13:33:29 +0000

    App2 3章

--- 5.App2/meshiterro/config/initializers/application_controller_renderer.rb
+++ 5.App2/meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '54bfd1adba8c5dd7bad26ef9648027eb8bdfdf49ad5a2650c3008d0e98354feb1adc4f72ec49ab229894713685789bb76a74e603ccf406221a3d41b684b23014'
