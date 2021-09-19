dcb02d3b1fe212b0407a3c21718b730d2c77c1c4
yonePC <ys.mailaddress0929@gmail.com>
2021-09-19 07:04:45 +0000

    App2 4章

--- 5.App2/meshiterro/app/controllers/post_images_controller.rb
+++ 5.App2/meshiterro/app/controllers/post_images_controller.rb
@@ -11,7 +11,7 @@ class PostImagesController < ApplicationController
   end
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.all.page(params[:page]).reverse_order
   end
 
   def show
--- 5.App2/meshiterro/db/schema.rb
+++ 5.App2/meshiterro/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_18_040009) do
+ActiveRecord::Schema.define(version: 2021_09_19_061130) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "post_comments", force: :cascade do |t|
     t.text "comment"
